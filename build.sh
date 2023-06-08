9c94fe1448509c8f1d9a092a6f57c7de9d8e0e58
Clément Baconnier <clement@baconnier.ch>
2023-06-08 08:59:52 +0200

    Cleanup

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 PHP_VERSION=$1
 PUBLISH=$2
 
-docker build -f ${PWD}/${PHP_VERSION}.Dockerfile -t vapor-${PHP_VERSION}:latest .
+docker buildx build -f ${PWD}/${PHP_VERSION}.Dockerfile -t vapor-buster-${PHP_VERSION}:latest .
 
-docker tag vapor-${PHP_VERSION}:latest laravelphp/vapor:${PHP_VERSION}
+docker tag vapor-buster-${PHP_VERSION}:latest melbach/vapor-buster:${PHP_VERSION}
 
 if [ -n "$PUBLISH" ]; then
-  docker push laravelphp/vapor:${PHP_VERSION}
+  docker push melba-ch/vapor-buster:${PHP_VERSION}
 fi
